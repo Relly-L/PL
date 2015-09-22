0adac466e1c486ac54fb58ab84d55342f51c8182
Relly-L <geralt.rest@gmail.com>
2015-09-22 16:27:19 +0300

    Added hover script for 'interest facts' panel

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
 	
 	$('.header').effect('slide', 1000);
+
+	$(".panel-heading").hover(
+		function () {
+		    $(this).addClass("class-for-background-interests",300);
+	    },
+	    function () {
+	 	   $(this).removeClass("class-for-background-interests",300);
+    	}
+);
+	
 });
